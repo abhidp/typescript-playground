3d2c47560e0179f1a51dc2a16c5485e4ddc9a8e1
abhidp <abhi358@gmail.com>
2019-07-29 19:14:49 +1000

    First Commit

---
+++ types.js
@@ -0,0 +1,26 @@
+// declare
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+//define
+myString = 'Hello Typescript';
+myNum = 22;
+myBool = false;
+myVar = 5;
+strArr = ['Hello', 'World', 'How', 'Are', 'You'];
+numArr = [1, 4, 667, 234, 23467];
+boolArr = [true, false, false, true, true];
+strNumTuple = ['Hello', 1];
+//execute
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(strArr);
+console.log(numArr);
+console.log(boolArr);
+console.log(strNumTuple);
---
+++ types.ts
@@ -0,0 +1,34 @@
+// declare
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar : any
+
+let strArr: string[]
+let numArr: number[]
+let boolArr: boolean[]
+
+let strNumTuple: [string, number]
+
+
+//define
+myString = 'Hello Typescript'
+myNum = 22
+myBool = false
+myVar = 5
+
+strArr = ['Hello', 'World', 'How', 'Are', 'You']
+numArr = [1, 4, 667, 234, 23467]
+boolArr = [true, false, false , true , true]
+strNumTuple =['Hello', 1 ]
+
+//execute
+console.log(myString)
+console.log(myNum)
+console.log(myBool)
+
+console.log(strArr)
+console.log(numArr)
+console.log(boolArr)
+
+console.log(strNumTuple)
